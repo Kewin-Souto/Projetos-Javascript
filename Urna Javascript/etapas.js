2fb0867d7e9dc437e454ac9a05b6a4b8fef27f44
Kewin Souto Barros <77506025+Kewin-Souto@users.noreply.github.com>
2021-04-29 17:33:38 -0300

    Urna em javascript

---
+++ Urna Javascript/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+
+        numero: '38111',
+        name: 'Craig',
+        partido: 'ABC',
+        fotos: [
+            
+                {url: 'Craig.png',
+                legenda: 'Vereador',
+                small: false}
+            
+        ]
+    },
+    {
+        numero: '77222',
+        nome: 'Token',
+        partido: 'DEFG',
+        fotos: [
+            { url: 'Token.jpg', legenda: 'Vereador' }
+        ]
+    },
+    ]
+},
+{
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [{
+
+        numero: '99',
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos: [
+            { url: 'Eric CArt.jpg', legenda: 'Prefeito' },
+            { url: 'Kenny Mcor.jfif', legenda: 'Vice-Prefeito', small: true }
+
+        ]
+    },
+
+    {
+        numero: '84',
+        nome: 'Zulano',
+        partido: 'ABC',
+        vice: 'Stan',
+        fotos: [
+            { url: 'Kaio Bro.jfif', legenda: 'Prefeito' },
+            { url: 'Stan Mars.jfif', legenda: 'Vice-Prefeito', small: true }
+        ]
+
+    },
+    ]
+}
+];
